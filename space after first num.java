6557df3f20a5e4060c78a95bcac490ed10329fbe
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-08-05 00:48:24 -0400

    Add files via upload

---
+++ space after first num.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        long n1=sc.nextLong();
+        long n2=sc.nextLong();
+        System.out.printf("%d%17d",n1,n2);
+    }
+}
