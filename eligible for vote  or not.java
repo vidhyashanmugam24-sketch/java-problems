2c0b70d31a70cd54e54ecee4dff290a273b4a76c
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-08-05 00:18:56 -0400

    Add files via upload

---
+++ eligible for vote  or not.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int age=sc.nextInt();
+	    if(age>=18){
+	        System.out.println("Eligible for vote");
+	    }
+	    else{
+	        System.out.println("Not eligible for vote");
+	    }
+		
+	}
+}
