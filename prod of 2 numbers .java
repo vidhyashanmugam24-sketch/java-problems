c8e711ee2bdc94f92ce386da6a42cac74861381a
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-08-05 00:36:57 -0400

    Add files via upload

---
+++ prod of 2 numbers .java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        long n1=sc.nextLong(),n2=sc.nextLong();
+        long result=n1*n2;
+        System.out.print(result);
+    }
+}
