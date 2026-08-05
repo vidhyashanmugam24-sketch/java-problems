8044b5bb05984da4807ce31a96b72e6faff59eb0
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-08-05 00:50:12 -0400

    Add files via upload

---
+++ precision.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        float n=sc.nextFloat();
+        int p=sc.nextInt();
+        System.out.printf("%."+p+"f",n);
+        
+    }
+}
