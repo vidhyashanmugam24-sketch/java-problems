3f80c22e7aa3ac983cf0f75e91dbb2c12c22a18a
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-08-05 00:07:39 -0400

    Add files via upload

---
+++ mul 1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int res=n*2;
+		System.out.print(res);
+
+	}
+}
