9e4567deb2fe728080d6958ba25da20c7203605b
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-08-05 00:01:25 -0400

    Add files via upload

---
+++ convert temperature.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double temperature=25.5;
+
+        System.out.println("Celsius - " + temperature);
+        System.out.println("Kelvin - " + (temperature + 273));
+        
+    }
+}
