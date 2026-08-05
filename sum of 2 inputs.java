c4ed75850c0bbfe87c70adc4bdfabfad6a4d14ad
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-08-05 00:35:45 -0400

    Add files via upload

---
+++ sum of 2 inputs.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        long n1=sc.nextLong();
+        long n2=sc.nextLong();
+        long result=n1+n2;
+        System.out.print(result);
+    }
+}
