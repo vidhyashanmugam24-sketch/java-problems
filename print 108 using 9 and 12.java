47f5a37fa91caa96184188adf06380a00e677869
vidhyashanmugam24-sketch <vidhyashanmugam24@gmail.com>
2026-08-04 23:53:49 -0400

    Add files via upload

---
+++ print 108 using 9 and 12.java
@@ -0,0 +1,8 @@
+class Codechef {
+    public static void main(String[] args) {
+        int a=9,b=12;
+        int result=9*12;
+        System.out.print(result);
+        
+    }
+}
---
+++ print.java
@@ -0,0 +1,8 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        System.out.print("Learn Coding on CodeChef");
+      
+	}
+}
